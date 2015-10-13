66c4bc01e18a2f42fb7e6a5c80f513e376d26342
mhossain <mohammad.hossain@sparkpost.com>
2015-10-13 17:30:50 -0400

    Bootstrap project

---
+++ lib/ruby/sparkpost.rb
@@ -0,0 +1,7 @@
+require "ruby/sparkpost/version"
+
+module Ruby
+  module Sparkpost
+    # Your code goes here...
+  end
+end
---
+++ lib/ruby/sparkpost/version.rb
@@ -0,0 +1,5 @@
+module Ruby
+  module Sparkpost
+    VERSION = "0.0.1"
+  end
+end
