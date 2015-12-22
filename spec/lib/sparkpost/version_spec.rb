dc48bf22eb412d2485e76135dfeed1eafe4ab995
Rich Leland <rich.leland@messagesystems.com>
2015-12-22 12:42:27 -0500

    Set version for initial release

--- spec/lib/sparkpost/version_spec.rb
+++ spec/lib/sparkpost/version_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 RSpec.describe 'Sparkpost' do
   it 'has a version' do
-    expect(SparkPost::VERSION).to eq('0.0.1')
+    expect(SparkPost::VERSION).to eq('0.1.0')
   end
 end
