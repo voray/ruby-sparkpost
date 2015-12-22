3695cb45081492b69d8407d5ad856e17ab358adb
mhossain <mohammad.hossain@sparkpost.com>
2015-12-22 14:05:20 -0500

    Bump version

--- spec/lib/sparkpost/version_spec.rb
+++ spec/lib/sparkpost/version_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 RSpec.describe 'Sparkpost' do
   it 'has a version' do
-    expect(SparkPost::VERSION).to eq('0.1.1')
+    expect(SparkPost::VERSION).to eq('0.1.2')
   end
 end
