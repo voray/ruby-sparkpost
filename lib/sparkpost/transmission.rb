6e6c234b208a82312f510e2442a11e92a23fd7ae
AimeeKnight <aimee.knight@messagesystems.com>
2015-12-29 18:21:35 -0500

    validate to and from emails

--- lib/sparkpost/transmission.rb
+++ lib/sparkpost/transmission.rb
@@ -19,13 +19,16 @@ module SparkPost
     end
 
     def send_message(to, from, subject, html_message = nil, **options)
-      #todo add validations for to, from
       unless to.is_a?(Array)
         to = [to]
       end
 
       if html_message.blank? && options[:text_message].blank?
         fail ArgumentError, 'Content missing. Either provide html_message or text_message in options parameter'
+      end
+
+      unless is_valid_email?(to) || is_valid_email?(from)
+        fail ArgumentError, 'Email is not valid'
       end
 
       options.merge!(
@@ -53,6 +56,10 @@ module SparkPost
             }
         }
       end
+    end
+
+    def is_valid_email?(email)
+      (email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i)
     end
 
   end
