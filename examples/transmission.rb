9497fda808e6b62fba9b42e91fb15cbbb7d0e8a2
mhossain <mohammad.hossain@sparkpost.com>
2015-12-30 15:13:16 -0500

    Remove unnecessary tranmission examples

--- examples/transmission.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-require_relative '../lib/sparkpost'
-
-sp = SparkPost::Client.new() # api key was set in ENV
-response = sp.transmission.send_message('RECIPIENT_EMAIL', 'SENDER_EMAIL', 'test email', '<h1>HTML message</h1>')
-
-puts response
