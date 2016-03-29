9f937156b1cd3e813c1efa12af16d9efa349c492
mhossain <mohammad.hossain@sparkpost.com>
2016-03-28 21:44:12 -0400

    Implement Api class

---
+++ lib/sparkpost/api.rb
@@ -0,0 +1,20 @@
+require_relative 'request'
+
+module SparkPost
+  class Api
+    include SparkPost::Request
+    PATH = ''
+    def initialize(api_key, api_host)
+      @api_key = api_key
+      @api_host = api_host
+    end
+
+    def api_url
+      @url ||= @api_host.concat(self.class::PATH)
+    end
+
+    def send_payload(data = {})
+      request(api_url, @api_key, data)
+    end
+  end
+end
