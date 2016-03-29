8f773bb5f4f0b7b97ccfe8d8c7a72c1b5aa8adaa
mhossain <mohammad.hossain@sparkpost.com>
2016-03-28 21:46:14 -0400

    Refactor template class

--- lib/sparkpost/template.rb
+++ lib/sparkpost/template.rb
@@ -1,25 +1,15 @@
 require 'net/http'
 require 'uri'
-require 'http'
 require_relative '../core_extensions/object'
-require_relative 'request'
+require_relative 'api'
 require_relative 'exceptions'
 
 module SparkPost
-  class Template
-    include Request
-
-    def initialize(api_key, api_host)
-      @api_key = api_key
-      @api_host = api_host
-    end
-
-    def endpoint
-      @endpoint ||= @api_host.concat('/api/v1/templates')
-    end
+  class Template < Api
+    PATH = '/api/v1/templates'
 
     def list
-      request(endpoint, @api_key, nil, 'GET')
+      request(api_url, @api_key, nil, 'GET')
     end
   end
 end
