347e4527ac3314d716ddc2a90f763cdadd654a50
Konstantin Sykulev <konst@sykulev.com>
2016-05-06 15:00:43 -0400

    Improve DeliveryException
    The sparkpost api returns errors in the following format:
    ```
    {
      "errors": [
        {
          "message": "Message generation rejected",
          "description": "recipient address suppressed due to customer policy",
          "code": "1902"
        }
      ]
    ...
    }
    
    ```
    
    `message`, `description`, and `code` are now available on the
    DeliveryException instance as: `service_message`,
    `service_description`, `service_code`
    
    The original message is preserved so it will not break any legacy
    parsing written by users.

--- lib/sparkpost/exceptions.rb
+++ lib/sparkpost/exceptions.rb
@@ -1,4 +1,17 @@
 module SparkPost
   class DeliveryException < Exception
+    attr_reader :service_message, :service_description, :service_code
+
+    def initialize(message)
+      errors = [*message].first
+
+      if errors.is_a?(Hash)
+        @service_message     = errors['message']
+        @service_description = errors['description']
+        @service_code        = errors['code']
+      end
+
+      super(message)
+    end
   end
 end
